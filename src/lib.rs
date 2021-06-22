c30acae116d61609bda12accebe759683fba51b3
orhun <orhun@archlinux.org>
2021-06-22 16:20:39 +0300

    feat: Add the initial implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use std::error::Error;
+use std::io::Write;
 
+const LEN: usize = 32;
+
+pub fn speak<W: Write>(words: Vec<String>, out: &mut W) -> Result<(), Box<dyn Error>> {
+    let words = words.choose_multiple(&mut thread_rng(), LEN);
+    for word in words {
+        write!(out, "{} ", word)?;
+    }
+    writeln!(out)?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use godsays::speak;
+use std::fs;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let words = fs::read_to_string("HAPPY.txt").expect("Unable to read HAPPY.txt");
+    if let Err(e) = speak(words.lines().map(String::from).collect(), &mut io::stdout()) {
+        eprintln!("Error: {:?}", e)
+    }
 }
