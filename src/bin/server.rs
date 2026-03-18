9fb6f6beabf9b1daaf3cc54fc53eb616023b29f0
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2026-03-19 00:46:00 +0300

    chore(deps): bump dependencies

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,52 +1,74 @@
 use godsays::God;
-use hyper::service;
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use bytes::Bytes;
+use http_body_util::Full;
+use hyper::body::Incoming;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Method, Request, Response, StatusCode};
+use hyper_util::rt::TokioIo;
 use std::convert::Infallible;
 use std::env;
 use std::error::Error;
+use tokio::net::TcpListener;
 
-#[tokio::main]
-pub async fn run_server(addr: &str, god: God) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let make_svc = service::make_service_fn(|_conn| {
-        let god = god.clone();
-        async move {
-            Ok::<_, Infallible>(service::service_fn(move |req: Request<Body>| {
-                let god = god.clone();
-                async move {
-                    match (req.method(), req.uri().path()) {
-                        (&Method::GET, "/") => Response::builder()
-                            .status(StatusCode::OK)
-                            .body(Body::from(god.speak())),
-                        (&Method::GET, "/json") => Response::builder()
-                            .header("Content-Type", "application/json")
-                            .status(StatusCode::OK)
-                            .body(Body::from(format!(
-                                r#"{{ "god_says": "{}" }}"#,
-                                god.speak()
-                            ))),
-                        _ => Response::builder()
-                            .status(StatusCode::NOT_FOUND)
-                            .body(Body::from("404")),
-                    }
-                }
-            }))
-        }
-    });
+type Body = Full<Bytes>;
 
-    let server = Server::bind(&addr.parse()?).serve(make_svc);
-    println!("Listening on http://{}", addr);
-    server.await?;
-
-    Ok(())
+fn response(status: StatusCode, body: String) -> Response<Body> {
+    Response::builder()
+        .status(status)
+        .body(Full::new(Bytes::from(body)))
+        .expect("response builder should be infallible for fixed bodies")
 }
 
-fn main() {
+fn json_response(body: String) -> Response<Body> {
+    Response::builder()
+        .header("Content-Type", "application/json")
+        .status(StatusCode::OK)
+        .body(Full::new(Bytes::from(body)))
+        .expect("response builder should be infallible for fixed bodies")
+}
+
+pub async fn run_server(addr: &str, god: God) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+        let god = god.clone();
+        tokio::spawn(async move {
+            let service = service_fn(move |req: Request<Incoming>| {
+                let god = god.clone();
+                async move {
+                    let response = match (req.method(), req.uri().path()) {
+                        (&Method::GET, "/") => response(StatusCode::OK, god.speak()),
+                        (&Method::GET, "/json") => {
+                            json_response(format!(r#"{{ "god_says": "{}" }}"#, god.speak()))
+                        }
+                        _ => response(StatusCode::NOT_FOUND, String::from("404")),
+                    };
+
+                    Ok::<_, Infallible>(response)
+                }
+            });
+
+            if let Err(error) = http1::Builder::new().serve_connection(io, service).await {
+                eprintln!("Connection error: {:?}", error);
+            }
+        });
+    }
+}
+
+#[tokio::main]
+async fn main() {
     pretty_env_logger::init();
     let god = God::init(32);
     if let Err(e) = run_server(
         &env::var("ADDR").unwrap_or_else(|_| String::from("127.0.0.1:3000")),
         god,
-    ) {
+    )
+    .await
+    {
         eprintln!("Server error: {:?}", e)
     }
 }
